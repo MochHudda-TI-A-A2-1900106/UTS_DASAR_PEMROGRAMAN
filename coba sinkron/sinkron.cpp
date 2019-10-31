08ebd16d65ef325485e700740cb90d13e357eec2
MochHudda <huddamoch21@gmail.com>
2019-10-31 10:18:09 +0700

    git@github.com:MochHudda-TI-A-A2-1900106/UTS_DASAR_PEMROGRAMAN.git

---
+++ coba sinkron/sinkron.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    
+    a = 10;
+    b = 5;
+    c = a + b;
+    cout << "Hasil dari 10 + 5 =" << c << endl;
+    
+    return 0;
+
+}
